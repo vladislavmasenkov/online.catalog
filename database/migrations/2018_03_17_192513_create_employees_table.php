60ab622150dd1b67ba2ba5ac1c9daa0c5e9f504d
vladislavmasenkov <vmasenkov2@gmail.com>
2018-03-18 15:15:47 +0200

    first commit

---
+++ database/migrations/2018_03_17_192513_create_employees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name',255);
+            $table->string('last_name',255);
+            $table->string('middle_name',255);
+            $table->integer('position');
+            $table->string('avatar',255);
+            $table->date('employment_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
